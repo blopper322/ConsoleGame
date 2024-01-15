12bc067b9ddf3b9eb3196ad1de12ce42b5ab3894
3petr3 <petrerebakan@mail.ru>
2024-01-15 13:35:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Привет странник!");
+        Console.Write("Как тебя звать ");
+        string playerName = Console.ReadLine();
+
+        Console.WriteLine($"\nВаше имя {playerName}!");
+
+        int playerHealth = 100;
+        int playerAttack = 20;
+        int medkitHeal = 10;
+        int medkits = 1;
+
+        Console.WriteLine($"Вам был выдан пистолет и 10 пуль ({playerAttack}), а также средняя аптечка ({medkitHeal}hp).\nУ вас {playerHealth}hp.");
+
+        string enemyName = "Бандит";
+        int enemyHealth = 40;
+        int enemyAttack = 10;
+
+        while (playerHealth > 0 && enemyHealth > 0)
+        {
+            Console.WriteLine($"\n{playerName} встречает врага {enemyName} ({enemyHealth}hp), у врага на в подсумке пистолет ({enemyAttack})");
+            Console.WriteLine("Что вы будете делать?");
+            Console.WriteLine("1. Убить его\n2. Пропустить ход\n3. Использовать аптечку");
+            Console.Write(">");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"\n{playerName} ударил противника {enemyName}");
+                    enemyHealth -= playerAttack;
+                    break;
+                case "2":
+                    Console.WriteLine($"\n{playerName} пропускает ход");
+                    break;
+                case "3":
+                    if (medkits > 0)
+                    {
+                        Console.WriteLine($"\n{playerName} использовал аптечку");
+                        playerHealth = Math.Min(playerHealth + medkitHeal, 100);
+                        medkits--;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nУ {playerName} нет аптечек!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("\nНеверный выбор. Попробуйте снова.");
+                    continue;
+            }
+
+            if (enemyHealth > 0)
+            {
+                Console.WriteLine($"{enemyName} ударил вас!");
+                playerHealth -= enemyAttack;
+            }
+
+            Console.WriteLine($"У противника {enemyHealth}hp, у вас {playerHealth}hp");
+        }
+
+        if (playerHealth > 0)
+        {
+            Console.WriteLine($"\n{playerName} победил!");
+        }
+        else
+        {
+            Console.WriteLine($"\n{playerName} был повержен...");
+        }
+    }
+}
